8463e67205525cd0fe93a0fb2deaa2d78b0f9aeb
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-10-21 09:36:28 +0530

    Calculator

---
+++ CalculatorLibJenkinsDemo/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibJenkinsDemo
+{
+    public class Calculator
+    {
+        int a, b, c;
+       
+        public void Add()
+        {
+            a = 2;
+            b = 3;
+
+            c = a + b;
+            Console.WriteLine(c);
+
+        }
+        public void Subtract()
+        {
+            a = 4;
+            b = 2;
+            c = a - b;
+            Console.WriteLine(c);
+        }
+
+        public void Multiply()
+        {
+            a = 2;
+            b = 2;
+            c = a * b;
+            Console.WriteLine(c);
+            
+        }
+        
+        
+    }
+    
+}
